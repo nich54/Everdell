449c3ba5e790ec923eaed01b301f1f0c73f735ab
Max Camp-Oberhauser <c.max@wustl.edu>
2024-02-25 12:20:00 -0800

    Automated setup better

--- PlayGame.py
+++ PlayGame.py
@@ -17,6 +17,21 @@ if (__name__ == "__main__"):
     shuffle_deck(startingDeck)
     deck = encrypt_deck(startingDeck)
 
+    first = ""
+    while (first != "y" and first != "n"):
+        first = input("Going first? (y/n) ")
+    
+    print("\nMEADOW:")
+    for _ in range(8):
+        draw_card(deck, startingDeck, False)
+    print("\nYOUR HAND:")
+    for _ in range(5 if first == "y" else 6):
+        draw_card(deck, startingDeck, False)
+    print("\nTHEIR HAND:")
+    for _ in range(6 if first == "y" else 5):
+        draw_card(deck, startingDeck, True)
+    print("")
+
     while (True):
         x = input("Draw a card? ")
         draw_card(deck, startingDeck, len(x) == 0)
